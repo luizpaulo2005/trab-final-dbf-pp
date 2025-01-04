119355c1c2a8d7776951db261089304fba80d0c8
luizpaulo2005 <luizpaulorodrigues2005@gmail.com>
2025-01-04 11:54:16 -0300

    feat: add categoria router to main application

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const { env } = require("./lib/env.js");
 const morgan = require("morgan");
 const cors = require("cors");
 const { helloRouter } = require("./router/hello.js");
+const categoriaRouter = require("./router/categoria.js");
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(morgan("dev"));
 app.use(cors());
 
 app.use("/hello", helloRouter);
+app.use("/categoria", categoriaRouter)
 
 app.listen(env.PORT, () => {
   console.log(`Server is running on port ${env.PORT}`);
