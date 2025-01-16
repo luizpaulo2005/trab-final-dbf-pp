81eccee6d4ee4d8a9dc7467143ae37d49a326739
luizpaulo2005 <luizpaulorodrigues2005@gmail.com>
2025-01-16 08:46:38 -0300

    feat: add public static folder config

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ app.set("view engine", "ejs");
 app.set("views", "./src/views");
 
 app.use(express.json());
+app.use(express.static("./public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 app.use(cors());
