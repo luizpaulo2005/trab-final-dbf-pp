b94f8164e0d766c6c92a372d13cace69994d1f5b
luizpaulo2005 <luizpaulorodrigues2005@gmail.com>
2025-01-23 18:19:30 -0300

    feat: set default value for PORT in environment configuration

--- src/lib/env.js
+++ src/lib/env.js
@@ -4,7 +4,8 @@ const envSchema = z.object({
   PORT: z.coerce
     .number()
     .min(3000, "Must be at least 3000")
-    .max(65535, "Must be at most 65535"),
+    .max(65535, "Must be at most 65535")
+    .default(3333)
 });
 
 const env = envSchema.parse(process.env);
